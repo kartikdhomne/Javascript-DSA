49c7749074fbdf41e515313bf24c50f520ed31a0
Kartik Dhomne <kartikdhomne1997@gmail.com>
2025-08-11 23:50:35 +0530

    "Solved 2nd largest and 2nd smallest element in array"

--- Array/Array-Basic/find-second-largest-without-sorting.js
+++ Array/Array-Basic/find-second-largest-without-sorting.js
@@ -1,12 +1,26 @@
-// Find the second largest and smallest element in an array
-
-//example :- const arr =[1,2,3,4,5] smallest-2/ largest-4
+// Find the second smallest and second largest element in an array
+// Example: arr = [1, 2, 3, 4, 5] => second smallest = 2, second largest = 4
 
 const arr = [3, 6, 2, 1, 5, 4];
+
 function findSmallestLargest(arr) {
-  let sortedArr = arr.sort((a, b) => a - b);
-  let secondMin = 
-  return sortedArr;
+  if (arr.length < 2) {
+    return "Array must have at least two elements";
+  }
+
+  // Sort array in ascending order
+  let sortedArr = arr.slice().sort((a, b) => a - b);
+
+  // Second smallest (at index 1)
+  let secondMin = sortedArr[1];
+
+  // Second largest (at second last index)
+  let secondMax = sortedArr[sortedArr.length - 2];
+
+  return {
+    secondSmallest: secondMin,
+    secondLargest: secondMax
+  };
 }
 
 console.log(findSmallestLargest(arr));
