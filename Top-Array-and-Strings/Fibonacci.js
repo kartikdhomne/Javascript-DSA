31078d4dffacff0ad4ecf9d6063e8d6dc038a598
Kartik Dhomne <kartikdhomne1997@gmail.com>
2025-10-02 17:26:11 +0530

    "Fibonacci using Recursion"

--- Top-Array-and-Strings/Fibonacci.js
+++ Top-Array-and-Strings/Fibonacci.js
@@ -9,10 +9,24 @@ const fib = function (n) {
     for (let i = 2; i <= n; i++) {
         arr.push(arr[i - 1] + arr[i - 2])
     }
-    return arr[5]
+    return arr[n]
 }
-fib(5)
+const res = fib(4)
+console.log(res)
 
 // arr.push -- Push the new value at the end of array.
 
 // Method 2 : Using Recursion
+
+// const fib2 = function (n) {
+//     if (n <= 1) return n;
+
+//     return fib2(n - 1) + fib2(n - 2);
+// }
+
+// or 
+
+// Arrow Function
+const fib2 = (n) => (n <= 1 ? n : fib2(n - 1) + fib2(n - 2))
+
+console.log(fib2(4))
