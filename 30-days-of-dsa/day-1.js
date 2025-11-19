9f4fbf36e9d07a52370285c786366712acc00eb5
Kartik Dhomne <kartikdhomne1997@gmail.com>
2025-11-19 23:53:17 +0530

    Remove duplicate

---
+++ 30-days-of-dsa/day-1.js
@@ -0,0 +1,46 @@
+// Que :- Remove Duplicate from array
+
+let arr = [2, 4, 6, 7, 9]; // sorted
+let arr1 = [5, 2, 7, 4, 6, 2] //unsorted
+
+
+// Method 1
+const removeDuplicate = (arr) => {
+    return [...new Set(arr)]
+}
+console.log(removeDuplicate(arr))
+console.log(removeDuplicate(arr1))
+
+//Method 2
+const removeDuplicateM2 = (arr) => {
+    let res = arr.filter((item, index) => arr.indexOf(item) === index)
+    return res
+}
+console.log(removeDuplicateM2(arr))
+console.log(removeDuplicateM2(arr1))
+
+
+
+
+// ************** Remove Duplicate From Object***************************
+
+let users = [
+    {
+        name: "kartik",
+        age: 27
+    },
+    {
+        name: "Pranjali",
+        age: 28
+    },
+    {
+        name: "kartik",
+        age: 28
+    }
+]
+
+const removeObjDuplicate = (users, key) => {
+    return [...new Map(users.map((item, index) => [key[item], item])).values()]
+}
+
+console.log(removeObjDuplicate)
