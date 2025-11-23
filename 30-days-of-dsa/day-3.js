d206619f39b6022e7426927cdec17709489e6b36
Kartik Dhomne <kartikdhomne1997@gmail.com>
2025-11-23 23:55:38 +0530

    "Moves zeros to end"

---
+++ 30-days-of-dsa/day-3.js
@@ -0,0 +1,30 @@
+// Moves Zeros at the end
+
+const arr = [1, 0, 2, 3, 0, 4, 0, 1];
+
+// Method 1 (Looping)
+function movesZero(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            arr[arr.length - 1].push();
+        }
+    }
+}
+
+
+// Method 2 (Using two pointers)
+function movesZero(arr) {
+    let left = 0
+
+    for (let right = 0; right < arr.length; right++) {
+        if (arr[right] != 0) {
+            let temp = arr[left]
+            arr[left] = arr[right]
+            arr[right] = temp;
+            left++;
+        }
+    }
+}
+
+movesZero(arr)
+console.log(arr);
